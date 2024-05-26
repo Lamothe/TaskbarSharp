1e7894c76e3450a72ecf39a019d3259aaa994d2c
Michael Lamothe <michael.lamothe@gmail.com>
2024-05-26 10:38:10 +1000

    Make sure that settings are properties.

--- TaskbarSharp/Settings.cs
+++ TaskbarSharp/Settings.cs
@@ -2,49 +2,49 @@
 {
     public class Settings
     {
-        public static bool Pause;
+        public static bool Pause{ get; set; }
 
-        public static int TaskbarStyle;
-        public static int SecondaryTaskbarStyle;
-        public static int PrimaryTaskbarOffset;
-        public static int SecondaryTaskbarOffset;
-        public static int CenterPrimaryOnly;
-        public static int CenterSecondaryOnly;
-        public static string AnimationStyle;
-        public static int AnimationSpeed;
-        public static int LoopRefreshRate;
-        public static int CenterInBetween;
-        public static int FixToolbarsOnTrayChange;
-        public static int SkipResolution;
-        public static int SkipResolution2;
-        public static int SkipResolution3;
-        public static int CheckFullscreenApp;
-        public static int DefaultTaskbarStyleOnWinMax;
-        public static int DontCenterTaskbar;
-        public static int HidePrimaryStartButton;
-        public static int HideSecondaryStartButton;
-        public static int HidePrimaryNotifyWnd;
-        public static int HideSecondaryNotifyWnd;
-        public static int ShowTrayIcon;
-        public static int TaskbarStyleOnMax;
-        public static int TaskbarStyleRed;
-        public static int TaskbarStyleGreen;
-        public static int TaskbarStyleBlue;
-        public static int TaskbarStyleAlpha;
-        public static int ConsoleEnabled;
-        public static int StickyStartButton;
-        public static int TotalPrimaryOpacity;
-        public static int TotalSecondaryOpacity;
-        public static int RevertZeroBeyondTray;
-        public static int TaskbarRounding;
-        public static int TaskbarSegments;
+        public static int TaskbarStyle{ get; set; }
+        public static int SecondaryTaskbarStyle{ get; set; }
+        public static int PrimaryTaskbarOffset{ get; set; }
+        public static int SecondaryTaskbarOffset{ get; set; }
+        public static int CenterPrimaryOnly{ get; set; }
+        public static int CenterSecondaryOnly{ get; set; }
+        public static string AnimationStyle{ get; set; }
+        public static int AnimationSpeed{ get; set; }
+        public static int LoopRefreshRate{ get; set; }
+        public static int CenterInBetween{ get; set; }
+        public static int FixToolbarsOnTrayChange{ get; set; }
+        public static int SkipResolution{ get; set; }
+        public static int SkipResolution2{ get; set; }
+        public static int SkipResolution3{ get; set; }
+        public static int CheckFullscreenApp{ get; set; }
+        public static int DefaultTaskbarStyleOnWinMax{ get; set; }
+        public static int DontCenterTaskbar{ get; set; }
+        public static int HidePrimaryStartButton{ get; set; }
+        public static int HideSecondaryStartButton{ get; set; }
+        public static int HidePrimaryNotifyWnd{ get; set; }
+        public static int HideSecondaryNotifyWnd{ get; set; }
+        public static int ShowTrayIcon{ get; set; }
+        public static int TaskbarStyleOnMax{ get; set; }
+        public static int TaskbarStyleRed{ get; set; }
+        public static int TaskbarStyleGreen{ get; set; }
+        public static int TaskbarStyleBlue{ get; set; }
+        public static int TaskbarStyleAlpha{ get; set; }
+        public static int ConsoleEnabled{ get; set; }
+        public static int StickyStartButton{ get; set; }
+        public static int TotalPrimaryOpacity{ get; set; }
+        public static int TotalSecondaryOpacity{ get; set; }
+        public static int RevertZeroBeyondTray{ get; set; }
+        public static int TaskbarRounding{ get; set; }
+        public static int TaskbarSegments{ get; set; }
 
-        public static int UseUIA;
+        public static int UseUIA{ get; set; }
 
         // If on battery TaskbarSharp will override with these
-        public static string OnBatteryAnimationStyle;
+        public static string OnBatteryAnimationStyle{ get; set; }
 
-        public static int OnBatteryLoopRefreshRate;
+        public static int OnBatteryLoopRefreshRate{ get; set; }
 
     }
 }
